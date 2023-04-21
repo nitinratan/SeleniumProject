347fc11df30d92b4e337d17577a928e063def966
Nitin Goswami <nitin.bgoswami92@gmail.com>
2023-04-21 17:36:23 +0530

    Added Flunt Wait

--- src/seleniumPackage/V_WaitMechanism.java
+++ src/seleniumPackage/V_WaitMechanism.java
@@ -2,14 +2,18 @@ package seleniumPackage;
 
 
 
+//import java.time.Duration;
 import java.util.concurrent.TimeUnit;
-
+import java.util.function.Function;
+import java.time.Duration;
+import java.util.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class V_WaitMechanism {
@@ -27,12 +31,26 @@ public class V_WaitMechanism {
 		driver.findElement(By.className("dropbtn")).click();
 
 		//Explicit Wait--> apply for one
-		WebDriverWait waait = new WebDriverWait(driver,30);
-		WebElement facebookLink= waait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Flipkart")));
-		facebookLink.click();
+		//WebDriverWait waait = new WebDriverWait(driver,30);
+	//	WebElement facebookLink= waait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Flipkart")));
+		//facebookLink.click();
+		// Fluent Wait 
+		 // Waiting 30 seconds for an element to be present on the page, checking
+		   // for its presence once every 5 seconds.
+		   Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+		       .withTimeout(Duration.ofSeconds(30))
+		       .pollingEvery(Duration.ofSeconds(5))
+		       .ignoring(NoSuchElementException.class);
+
+		   WebElement facebookElement = wait.until(new Function<WebDriver, WebElement>() {
+		     public WebElement apply(WebDriver driver) {
+		       return driver.findElement(By.linkText("Flipkart"));
+		     }
+		   });
 		
-	//	driver.findElement(By.linkText("Flipkart")).click();
-		System.out.println("Hello");
+		//driver.findElement(By.linkText("Flipkart")).click();
+		   facebookElement.click();
+		   System.out.println("Hello");
 
 		driver.quit();
 
