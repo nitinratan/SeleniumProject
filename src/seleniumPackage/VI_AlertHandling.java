55de9187155f05061f19ae89d9acd0c0d2af9bd7
Nitin Goswami <nitin.bgoswami92@gmail.com>
2023-04-21 18:15:04 +0530

    Alert handling java file added

---
+++ src/seleniumPackage/VI_AlertHandling.java
@@ -0,0 +1,34 @@
+package seleniumPackage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class VI_AlertHandling {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\JavaProgram WorkSpace\\SeleniumProject\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://omayo.blogspot.com/");
+		driver.manage().window().maximize();
+		// implicit wait--> apply in all element --> good practics to put 15 sec implicit wait globally
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+		driver.findElement(By.id("alert1")).click();
+		//Switching control to alert
+		Alert al = driver.switchTo().alert();
+		//Getting tyext from alert
+		System.out.println(al.getText());
+		//Accepting the alert
+		al.accept();
+		//Not accepting Alert
+		al.dismiss();
+		System.out.println("Test");
+		driver.close();
+	}
+
+}
--- src/seleniumPackage/V_WaitMechanism.java
+++ src/seleniumPackage/V_WaitMechanism.java
@@ -1,7 +1,5 @@
 package seleniumPackage;
 
-
-
 //import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
@@ -25,32 +23,31 @@ public class V_WaitMechanism {
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://omayo.blogspot.com/");
 		driver.manage().window().maximize();
+		// implicit wait--> apply in all element --> good practics to put 15 sec implicit wait globally
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
-		// implicit wait--> apply in all element
-		 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.findElement(By.className("dropbtn")).click();
 
-		//Explicit Wait--> apply for one
-		//WebDriverWait waait = new WebDriverWait(driver,30);
-	//	WebElement facebookLink= waait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Flipkart")));
-		//facebookLink.click();
-		// Fluent Wait 
-		 // Waiting 30 seconds for an element to be present on the page, checking
-		   // for its presence once every 5 seconds.
-		   Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
-		       .withTimeout(Duration.ofSeconds(30))
-		       .pollingEvery(Duration.ofSeconds(5))
-		       .ignoring(NoSuchElementException.class);
+		// Explicit Wait--> apply for one
+		// WebDriverWait waait = new WebDriverWait(driver,30);
+		// WebElement facebookLink=
+		// waait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Flipkart")));
+		// facebookLink.click();
+		// Fluent Wait
+		// Waiting 30 seconds for an element to be present on the page, checking
+		// for its presence once every 5 seconds.
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(30))
+				.pollingEvery(Duration.ofSeconds(5)).ignoring(NoSuchElementException.class);
 
-		   WebElement facebookElement = wait.until(new Function<WebDriver, WebElement>() {
-		     public WebElement apply(WebDriver driver) {
-		       return driver.findElement(By.linkText("Flipkart"));
-		     }
-		   });
-		
-		//driver.findElement(By.linkText("Flipkart")).click();
-		   facebookElement.click();
-		   System.out.println("Hello");
+		WebElement facebookElement = wait.until(new Function<WebDriver, WebElement>() {
+			public WebElement apply(WebDriver driver) {
+				return driver.findElement(By.linkText("Flipkart"));
+			}
+		});
+
+		// driver.findElement(By.linkText("Flipkart")).click();
+		facebookElement.click();
+		System.out.println("Hello");
 
 		driver.quit();
 
